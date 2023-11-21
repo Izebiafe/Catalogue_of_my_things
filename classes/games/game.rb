c7684b6ae0c1e25e3118a65cffdc5b32ac7b76b4
PATRICIA CHRISTELLE MANEZEU <p.manezeu@group-activa.com>
2023-11-21 16:03:33 +0100

    Added the game class

--- classes/games/game.rb
+++ classes/games/game.rb
@@ -3,7 +3,15 @@ require_relative '../items/item'
 class Game < Item
   attr_accessor :multiplayer, :last_played_at
 
-  def initialize(publish_date, id = nil)
-    super(publish_date, id = id)
-    
+  def initialize(publish_date, id = nil, last_played_at = nil, archived: false, multiplayer: false)
+    super(publish_date, id, archived: archived)
+    @multiplayer = multiplayer
+    @last_played_at = last_played_at
   end
+
+  def can_be_archived?
+    current_year = Date.today.year
+    last_played_year = Date.parse(@last_played_at).year
+    super && current_year - last_played_year > 2
+  end
+end
